74d340bfd4ff54c3d482366b35a0d08374e33a8a
Andre Teixeira <andre.teixeira@gmail.com>
2017-01-25 14:06:57 -0200

    Installing and configuring webpack json-loader

--- src/components/cv.js
+++ src/components/cv.js
@@ -3,44 +3,24 @@ import React from 'react';
 import Info from "./cv/info"
 import WorkExperience from "./cv/workexperience"
 import AwardsSkills from "./cv/awardskills"
- 
-var EXPERIENCES = [
-	{
-		"id": 2,
-		"from": "2003-01", 
-		"until": "2004-12",
-		"title": "FrontEnd Engineer",
-		"hashtags": ['Android', 'iOS', 'reactJS'],
-		"location": 'Urbanizo – Brasília, DF, Brazil',
-		"contact_name": "Marcos Beto",
-		"contact_phone": "+55 61 98158-5100",
-		"information": "Very strong Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis turpis ac porttitor malesuada. Donec eu vehicula tortor. Nulla scelerisque augue turpis, vel vestibulum turpis gravida id. Maecenas vel efficitur ligula."
-	},
-	{
-		"id": 1,
-		"from": "2003-01", 
-		"until": "2004-12",
-		"title": "FrontEnd Engineer",
-		"hashtags": ['Android', 'iOS', 'reactJS'],
-		"location": 'Urbanizo – Brasília, DF, Brazil',
-		"contact_name": "Marcos Beto",
-		"contact_phone": "+55 61 98158-5100",
-		"information": "Very strong Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis turpis ac porttitor malesuada. Donec eu vehicula tortor. Nulla scelerisque augue turpis, vel vestibulum turpis gravida id. Maecenas vel efficitur ligula."
-	}
-];
+
+import data from "../data/seed"
+
 
 module.exports = React.createClass({
 
 
     // RENDER
     render: function() {
+
+    	console.log(data);
         
         return (
             <div className="cv">
                 <div className="box">
                     <h1>CURRICULUM VITAE</h1>
                    	<Info />
-                   	<WorkExperience experiences={EXPERIENCES}/>
+                   	<WorkExperience experiences={data.EXPERIENCES}/>
                    	<AwardsSkills />
                 </div>
                 <br/>
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -37,6 +37,9 @@ var webpackConfig = {
             },{ 
                 test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
                 loader: 'file'
+            },{
+                test: /\.json$/,
+                loader: 'json-loader'
             }
         ]
     },
