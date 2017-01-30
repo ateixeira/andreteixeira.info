90a91d86c080fc6f8a719b14cfd301125019811f
Andre Teixeira <andre.teixeira@gmail.com>
2017-01-30 20:41:12 -0200

    Creating an IndexRoute on react-router configuration.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 
 import Style from './styles/style.scss';
 
 import App from './components/app.js';
+import Home from './components/home.js';
 import Blog from './components/blog.js';
 import Dashboard from './components/dashboard.js';
 import Cv from './components/cv.js';
@@ -13,6 +14,7 @@ render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       {/* make them children of `App` */}
+      <IndexRoute component={Home}/>
       <Route path="/blog" component={Blog}/>
       <Route path="/dashboard" component={Dashboard}/>
       <Route path="/cv" component={Cv}/>
